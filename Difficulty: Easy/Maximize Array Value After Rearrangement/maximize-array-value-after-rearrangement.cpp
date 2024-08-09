9bda88d47171d5e2a085ec06bd449bd44839598d
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-09 19:03:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximize Array Value After Rearrangement/maximize-array-value-after-rearrangement.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int Maximize(vector<int> &arr) {
+        // Complete the function
+        sort(arr.begin(),arr.end());
+        long long ans=0;
+        int mod=1e9+7;
+        for(int i=0;i<arr.size();i++)
+        {
+            ans= ans + (long long) arr[i]*i;
+        }
+        return ans%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        //  cin.ignore();
+        int n = arr.size();
+        Solution ob;
+        cout << ob.Maximize(arr) << endl;
+    }
+}
+// } Driver Code Ends
