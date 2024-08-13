479ad06a212e9f41c633b98a0ac1158d352fd834
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-08-13 11:40:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check perfect square/check-perfect-square.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int isPerfectSquare(long long int n){
+        // code here
+        long long int sq=sqrtl(n);
+        if(sq*sq==n) return 1;
+        else return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.isPerfectSquare(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
