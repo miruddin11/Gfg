5b8090cab3d0e33444cd824c5c25f179466e8558
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-11-12 23:25:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Meeting Rooms/meeting-rooms.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Your Code Here
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        int endTime=arr[0][1];
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i][0]<endTime){
+                return false;
+            }
+            endTime=arr[i][1];
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> arr(n);
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            arr[i].push_back(x);
+            arr[i].push_back(y);
+        }
+        cin.ignore();
+        Solution obj;
+        bool ans = obj.canAttend(arr);
+        if (ans)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
