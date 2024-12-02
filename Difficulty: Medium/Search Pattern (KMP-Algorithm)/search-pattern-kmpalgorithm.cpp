b7966d10b877e812625460bf9c5c058d0e25ac7d
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2024-12-02 22:23:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> search(string& pat, string& txt) {
+        // code here
+        vector<int> ans;
+        int n = pat.length();
+        for(int i=0; i<txt.length()-n+1; i++) {
+            if(pat[0] != txt[i])  continue;
+            string str = txt.substr(i, n);
+            if(str == pat)  ans.push_back(i);
+       }
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector<int> res = ob.search(pat, S);
+        if (res.size() == 0)
+            cout << "[]" << endl;
+        else {
+            for (int i : res)
+                cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
