d154907535c6a667f0c31636624d4b9307b3e9e4
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-02-24 23:32:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+           stack<int>st;
+       vector<int>ans(arr.size(),1);
+       st.push(0);
+       for(int i=1;i<arr.size();i++){
+           
+           while(!st.empty() && arr[i]>=arr[st.top()]){
+               ans[i]+=ans[st.top()];
+               st.pop();
+           }
+           st.push(i);
+       }
+       return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.calculateSpan(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
