646b8381dc9ad423ba696aa3a486751bb18f1792
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-03-07 11:59:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -1,26 +1,25 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
 
-class Solution{
+// User function Template for C++
+
+class Solution {
   public:
-    int longestPalinSubseq(string s) {
-        //code here
-        string t=s;
-        reverse(t.begin(),t.end());
-        int n=s.size(),m=t.size();
+    int lcs(string &str1, string &str2) {
+        // code here
+        int n=str1.size(),m=str2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(s[i-1]==t[j-1]){
+                if(str1[i-1]==str2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
@@ -30,20 +29,29 @@ class Solution{
         }
         return dp[n][m];
     }
+    int longestPalinSubseq(string &s) {
+        // code here
+        string t=s;
+        reverse(s.begin(),s.end());
+        return lcs(s,t);
+    }
 };
+
 
 //{ Driver Code Starts.
 
-int32_t main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        string s; cin >> s;
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
         Solution ob;
         cout << ob.longestPalinSubseq(s) << endl;
+
+        cout << "~"
+             << "\n";
     }
 }
-// Contributed By: Pranay Bansal
 
 // } Driver Code Ends
