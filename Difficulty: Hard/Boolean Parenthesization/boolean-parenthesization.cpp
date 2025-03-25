39a1a6e3f9c0695d2d9b4726d66da5f7d6431715
Mir Tariquddin <155989881+miruddin11@users.noreply.github.com>
2025-03-25 19:04:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Boolean Parenthesization/boolean-parenthesization.cpp
+++ Difficulty: Hard/Boolean Parenthesization/boolean-parenthesization.cpp
@@ -1,16 +1,14 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-// User function Template for C++
 
-class Solution{
-public:
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
     vector<vector<vector<long long>>> dp;
-    int mod=1003;
     int solve(string &s,int i,int j,bool isTrue)
     {
         if(i>j) return 0;
@@ -29,41 +27,40 @@ public:
             long long rightT=solve(s,k+1,j,true);
             long long rightF=solve(s,k+1,j,false);
             if(s[k]=='|'){
-                if(isTrue) ways=(ways%mod+leftT*rightF + leftF*rightT+ leftT*rightT)%mod;
-                else ways=(ways%mod+leftF*rightF)%mod;
+                if(isTrue) ways=(ways+leftT*rightF + leftF*rightT+ leftT*rightT);
+                else ways=(ways+leftF*rightF);
             }
             else if(s[k]=='&'){
-                if(isTrue) ways= (ways%mod+leftT*rightT)%mod;
-                else ways= (ways%mod+leftT*rightF + leftF*rightT + leftF*rightF)%mod;
+                if(isTrue) ways= (ways+leftT*rightT);
+                else ways= (ways+leftT*rightF + leftF*rightT + leftF*rightF);
             }
             else if(s[k]=='^'){
-                if(isTrue) ways=(ways%mod+leftT*rightF + leftF*rightT)%mod;
-                else ways= (ways%mod+leftT*rightT + leftF*rightF)%mod;
+                if(isTrue) ways=(ways+leftT*rightF + leftF*rightT);
+                else ways= (ways+leftT*rightT + leftF*rightF);
             }
         }
-        return dp[i][j][isTrue]=ways%mod;
+        return dp[i][j][isTrue]=ways;
     }
-    int countWays(int n, string s){
+    int countWays(string &s) {
         // code here
+        int n=s.size();
         dp.resize(n,vector<vector<long long>>(n,vector<long long>(2,-1)));
         return solve(s,0,n-1,true);
     }
 };
 
-//{ Driver Code Starts.
 
-int main(){
+//{ Driver Code Starts.
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
+    cin >> t;
+    while (t--) {
         string s;
-        cin>>s;
-        
+        cin >> s;
         Solution ob;
-        cout<<ob.countWays(n, s)<<"\n";
+        int ans = ob.countWays(s);
+        cout << ans << endl;
+        cout << "~" << endl;
     }
-    return 0;
 }
 // } Driver Code Ends
